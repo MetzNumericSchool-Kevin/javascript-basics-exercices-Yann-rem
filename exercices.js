55e999220a1b4a4fbba80fe696eb32b7ef439ddc
Yannick Remy <remyyannick@sfr.fr>
2025-01-22 09:05:06 +0100

    Exercice 2 : Affichage conditionnel

--- exercices.js
+++ exercices.js
@@ -7,3 +7,13 @@ const prixPotion = 10;
 // Déclaration des variables
 let nbPotions = 10;
 let estOuverte = false;
+
+// Exercice 2 : Affichage conditionnel
+
+// Gestion du message via un conditionnement
+const msg = estOuverte
+  ? `Bienvenue dans la boutique ${nomBoutique} Aventurier ! 🎉`
+  : `La boutique ${nomBoutique} est fermée, revenez plus tard Aventurier ! 😴`;
+
+// Impression du message dans la console
+console.log(msg);
