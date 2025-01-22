fbbeae51c2f78463fe358e9ec11c799fc602bda5
Yannick Remy <remyyannick@sfr.fr>
2025-01-22 09:22:33 +0100

    Exercice 3 : Affichage conditionnel avec switch

--- exercices.js
+++ exercices.js
@@ -17,3 +17,37 @@ const msg = estOuverte
 
 // Impression du message dans la console
 console.log(msg);
+
+// Exercice 3 : Affichage conditionnel avec switch
+
+// Informtions
+console.log(
+  "Bienvenue dans mon humble boutique Aventurier. Que veux-tu savoir ? 🤔\n" +
+    "1. Le nom de la boutique\n" +
+    "2. Le nom du Sorcier\n" +
+    "3. Le prix d'une potion de soin\n" +
+    "4. La quantité d'une potion de soin"
+);
+
+// Prompt
+const choix = parseInt(prompt("Quel est ton choix ? 🤔"));
+
+// Gestion du choix avec `switch`
+switch (choix) {
+  case 1:
+    console.log("Le nom de la boutique");
+    break;
+  case 2:
+    console.log("Le nom du Sorcier");
+    break;
+  case 3:
+    console.log("Le prix d'un potion de soin");
+    break;
+  case 4:
+    console.log("La quantité d'une potion de soin");
+    break;
+  default:
+    console.log(
+      "Mh... Désolé aventurier, je ne comprends pas ce que tu souhaites. Refais ton choix ! 😕"
+    );
+}
