4b505f5c93aaa8c0622d7d9d9488ff8b402a4d8f
Yannick Remy <remyyannick@sfr.fr>
2025-01-22 09:01:25 +0100

    Exercice 1 : Définition de variables

---
+++ exercices.js
@@ -0,0 +1,9 @@
+//Exercice 1 : Définition de variables
+
+// Déclaration des constantes
+const nomBoutique = "Vesuve";
+const prixPotion = 10;
+
+// Déclaration des variables
+let nbPotions = 10;
+let estOuverte = false;
